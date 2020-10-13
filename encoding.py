aa1453bfac0599179ea8a2210064b2ae9ce9109f
NevinoSus <70443901+NevinoSus@users.noreply.github.com>
2020-10-13 08:20:13 +0100

    Create encoding.py

---
+++ encoding.py
@@ -0,0 +1,11 @@
+# unicode string
+string = 'pythön!'
+
+# print string
+print('The string is:', string)
+
+# default encoding to utf-8
+string_utf = string.encode()
+
+# print result
+print('The encoded version is:', string_utf)
