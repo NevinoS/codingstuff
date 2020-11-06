fc0afa9b7f81e939bca74f72ef5b465c0bdf71b2
NevinoSus <70443901+NevinoSus@users.noreply.github.com>
2020-11-06 18:28:26 +0000

    Update yousaywhichway.py

--- yousaywhichway.py
+++ yousaywhichway.py
@@ -1,6 +1,7 @@
-import sys, os
+import sys, os, requests
 import random as r
 from time import sleep
+from bs4 import BeautifulSoup
 
 classes = ['Mage', 'Berserker', 'Healer', 'Archer', 'Tank']
 prompt = '>>> '
@@ -13,16 +14,23 @@ pagenum2 = r.randint(10,15)
 pagenum3 = r.randint(15,20)
 tries = 0
 
+dateweb = requests.get('https://www.timeanddate.com/').text
+soup1 = BeautifulSoup(dateweb, 'lxml')
+
+date = soup1.find('span', {'id' : 'ij2'}).text
+
 # --------------------------------------------------------------------------------------------------
 
 def write(string):
     for char in string:
         sys.stdout.write(char)
         sys.stdout.flush()
-        sleep(0.05)
+        sleep(0.02)
     print()
 
-print('তততততততততততততততততততততততততততততততততততততততততততততততততততততততততততততততততততত')
+print()
+print()
+print('তততততততততততততততততততততততততততততততততততততততততততততততততততততততততততততততততততততততততততত')
 print('                                                                                               ')
 print('                                                                                               ')
 print('    ✧･ﾟ: *✧･ﾟ:* 　　 *:･ﾟ✧*:･ﾟ✧              ✧･ﾟ: *✧･ﾟ:* 　　 *:･ﾟ✧*:･ﾟ✧          ')
@@ -32,13 +40,14 @@ print('                                                                         
 print('    ✧･ﾟ: *✧･ﾟ:* 　　 *:･ﾟ✧*:･ﾟ✧              ✧･ﾟ: *✧･ﾟ:* 　　 *:･ﾟ✧*:･ﾟ✧          ')
 print('                                                                                               ')
 print('                                                                                               ')
-print('তততততততততততততততততততততততততততততততততততততততততততততততততততততততততততততততততততত')
+print('তততততততততততততততততততততততততততততততততততততততততততততততততততততততততততততততততততততততততততত')
+print()
 print()
 
 
 sleep(2)
 write('Please enter your desired character name below.')
-name = input(prompt)
+name = input(prompt).strip()
 print()
 sleep(2)
 write(f'Hello {name}!')
@@ -52,6 +61,7 @@ sleep(2)
 print()
 write(f'Your generated class is {randclass}!')
 print()
+sleep(3)
 if randclass == 'Mage':
     health += 85
     strength += 25
@@ -63,7 +73,7 @@ if randclass == 'Mage':
     write('Intelligence: 125')
     write('Special: Mage Staff')
     print()
-    sleep(4)
+    sleep(6)
 elif randclass == 'Berserker':
     health += 115
     strength += 75
@@ -75,7 +85,7 @@ elif randclass == 'Berserker':
     write('Intelligence: 15')
     write('Special: Me Smash Head')                                      # It works!!! Now health can be depleted, or added to throughout the program, just like other player stats.
     print()
-    sleep(4)
+    sleep(6)
 elif randclass == 'Healer':
     health += 105
     strength += 35
@@ -87,7 +97,7 @@ elif randclass == 'Healer':
     write('Intelligence: 65')
     write('Special: Wish')
     print()
-    sleep(4)
+    sleep(6)
 elif randclass == 'Archer':
     health += 95
     strength += 35
@@ -99,7 +109,7 @@ elif randclass == 'Archer':
     write('Intelligence: 50')
     write('Special: Extreme Focus')
     print()
-    sleep(4)
+    sleep(6)
 else:
     health += 185
     strength += 45
@@ -111,33 +121,30 @@ else:
     write('Intelligence: 25')
     write('Special: Fortify')
     print()
-    sleep(4)
-
-
-def playercard():
-    print()
-    print('Player Info:')
-    print(f'Name: {name}')                    # They should be able to ask for their playercard at any point throughout the program!
-    print(f'Class: {randclass}')
-    print(f'Current Health:  {health}')
-    print(f'Current Strength: {strength}')
-    print(f'Current Intelligence: {intelligence}')
+    sleep(6)
 
 print()
-write('Here is your playercard. You can view it at any point by entering "playercard" into the terminal!')
+write('Here is your playercard!')
 print()
-playercard()
+print('Player Info:')
+print(f'Name: {name}')
+print(f'Class: {randclass}')
+print(f'Current Health:  {health}')
+print(f'Current Strength: {strength}')
+print(f'Current Intelligence: {intelligence}')
 print()
+sleep(4)
 write(f'Here we start our journey {name}. YouSayWhichWay is a combination of chance and choice. Your character explores for you, but you have to deal with what happens to that character on their way. Your journey begins here. You character finds a book. Please turn to page {pagenum1} of the YouSayWhichWay manual.')
 print()
 
-sleep(5)
+sleep(7)
 if pagenum1 == 1:
     write(f'You have turned to page {pagenum1}! Enter your solution to the page below.')
     while True:
         oneans = input(prompt).lower().strip()
         if oneans in ['yes']:
             write(f'Correct! The answer was {oneans}')
+            break
         elif oneans not in ['yes']:
             tries += 1
             write(f'Incorrect! You have used {tries}/3 tries.')
@@ -150,6 +157,7 @@ elif pagenum1 == 2:
         twoans = input(prompt).lower().strip()
         if twoans in ['yes']:
             write(f'Correct! The answer was {twoans}')
+            break
         elif twoans not in ['yes']:
             tries += 1
             write(f'Incorrect! You have used {tries}/3 tries.')
@@ -162,6 +170,7 @@ elif pagenum1 == 3:
         threeans = input(prompt).lower().strip()
         if threeans in ['yes']:
             write(f'Correct! The answer was {threeans}')
+            break
         elif threeans not in ['yes']:
             tries += 1
             write(f'Incorrect! You have used {tries}/3 tries.')
@@ -174,6 +183,7 @@ elif pagenum1 == 4:
         fourans = input(prompt).lower().strip()
         if fourans in ['yes']:
             write(f'Correct! The answer was {fourans}')
+            break
         elif fourans not in ['yes']:
             tries += 1
             write(f'Incorrect! You have used {tries}/3 tries.')
@@ -186,6 +196,7 @@ elif pagenum1 == 5:
         fiveans = input(prompt).lower().strip()
         if fiveans in ['yes']:
             write(f'Correct! The answer was {fiveans}')
+            break
         elif fiveans not in ['yes']:
             tries += 1
             write(f'Incorrect! You have used {tries}/3 tries.')
@@ -198,6 +209,7 @@ elif pagenum1 == 6:
         sixans = input(prompt).lower().strip()
         if sixans in ['yes']:
             write(f'Correct! The answer was {sixans}')
+            break
         elif sixans not in ['yes']:
             tries += 1
             write(f'Incorrect! You have used {tries}/3 tries.')
@@ -210,6 +222,7 @@ elif pagenum1 == 7:
         sevenans = input(prompt).lower().strip()
         if sevenans in ['yes']:
             write(f'Correct! The answer was {sevenans}')
+            break
         elif sevenans not in ['yes']:
             tries += 1
             write(f'Incorrect! You have used {tries}/3 tries.')
@@ -222,6 +235,7 @@ elif pagenum1 == 8:
         eightans = input(prompt).lower().strip()
         if eightans in ['yes']:
             write(f'Correct! The answer was {eightans}')
+            break
         elif eightans not in ['yes']:
             tries += 1
             write(f'Incorrect! You have used {tries}/3 tries.')
@@ -234,6 +248,7 @@ elif pagenum1 == 9:
         nineans = input(prompt).lower().strip()
         if nineans in ['yes']:
             write(f'Correct! The answer was {nineans}')
+            break
         elif nineans not in ['yes']:
             tries += 1
             write(f'Incorrect! You have used {tries}/3 tries.')
@@ -246,9 +261,22 @@ else:
         tenans = input(prompt).lower().strip()
         if tenans in ['yes']:
             write(f'Correct! The answer was {tenans}')
+            break
         elif tenans not in ['yes']:
             tries += 1
             write(f'Incorrect! You have used {tries}/3 tries.')
             if tries == 3:
                 write('Oh No! It is the end of the road for you friend. Game ended, You Died.')
                 break
+
+f = open('stats.txt', 'a')
+f.write('--------------------------------------\n')
+f.write('Player Info:\n')
+f.write(f'Name: {name}\n')
+f.write(f'Class: {randclass}\n')
+f.write(f'Final Health:  {health}\n')
+f.write(f'Final Strength: {strength}\n')
+f.write(f'Final Intelligence: {intelligence}\n')
+f.write('-\n')
+f.write(f'Date completed: {date}.\n')
+f.write('--------------------------------------\n')
